bd2c65995b5f05e606688d74dfba911d707ebee1
Sefa Aslan <sefa@MacBook-Air-von-Sefa.local>
2025-05-11 22:37:15 +0200

    Nach Uebung1

--- javasrc/de/htwberlin/dbtech/aufgaben/ue02/VersicherungJdbc.java
+++ javasrc/de/htwberlin/dbtech/aufgaben/ue02/VersicherungJdbc.java
@@ -11,7 +11,11 @@ import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -36,9 +40,26 @@ public class VersicherungJdbc implements IVersicherungJdbc {
 
     @Override
     public List<String> kurzBezProdukte() {
-        L.info("start");
-        L.info("ende");
-        return null;
+
+        List<String> kurzBez = new LinkedList<String>();
+        String query = "SELECT kurzbez from produkt order by id asc";
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try{
+             ps = useConnection().prepareStatement(query);
+
+             rs = ps.executeQuery();
+             while (rs.next()) {
+                 kurzBez.add(rs.getString("kurzbez"));
+             }
+        }
+        catch (SQLException e) {
+            throw new DataException(e);
+
+        }
+
+        return kurzBez;
     }
 
     @Override
@@ -64,5 +85,16 @@ public class VersicherungJdbc implements IVersicherungJdbc {
         L.info("ende");
         return null;
     }
+
+     /* "Man kann mit Methoden erweitern"
+    public boolean isPrimKeyExisting(int id, String tablename){
+        boolean isExisting = false;
+
+        //JDBC Zugriff
+
+        return isExisting;
+    }
+
+    bis hier nur */
 
 }
